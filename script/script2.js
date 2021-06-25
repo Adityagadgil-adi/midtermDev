efbc0265645a2bf3891220b1d4eb76688516aeb9
Aditya Gadgil <gadgil@sheridancollege.com>
2021-06-25 14:53:41 -0400

    commit

--- script/script2.js
+++ script/script2.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    var arr = [];
+    
     $("#info").append("<br>"); 
     $("#info").append("<br>"); 
     for (var i = 0; i < localStorage.length; i++) {
-        arr.push(localStorage.key(i));
+        
         $("#info").append("<button id='abc' onclick = 'myfunction()' value ="+localStorage.key(i)+">"+localStorage.key(i)+" </button>" + " <br>"); 
     }
     
--- script/script3.js
+++ script/script3.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
    
     $("#info").append("<br>"); 
     $("#info").append("<br>"); 
-
+    
     for (var i = 0; i < localStorage.length; i++) {
         
-        $("#info").append(localStorage.key(i)); 
-        $("#info").append(localStorage.getItem($("abc").value)); 
+        $("#info").append(localStorage.getItem(localStorage.key(i))); 
+        
 
     }
     
